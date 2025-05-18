eecf206c70af7bcc540abed8450f83fa40d35cdd
NaveedKhan09 <naveedkhan10990@gmail.com>
2025-05-19 07:51:03 +0800

    deployment to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const base = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/portfolio/' : '/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/portfolio"
+  base,
 })
